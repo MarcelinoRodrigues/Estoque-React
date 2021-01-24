e57ed9be52f43f6ee4c8352d6e538802b58bae8d
Marcelino Rodrigues <marcelino.rodrigues.d.n@gmail.com>
2021-01-24 19:26:06 -0300

    first commit

---
+++ src/components/button.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Button = (props) => <button>{props.label}</button>
+
+export default Button;
---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import react from 'react'
+
+const Home = (query) =>(
+    <form>
+        <input type="text" placeholder="User"/>
+        <input type="password" placeholder="Password"/>
+    </form>
+)
+
+export default Home;
